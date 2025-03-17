70be7e30439fa510dc558af1df2758b038178127
Jeremy Chone <jeremy.chone@gmail.com>
2025-03-16 18:51:56 -0700

    > refactor aip_modules code layout

---
+++ src/script/lua_script/aip_modules/mod.rs
@@ -0,0 +1,20 @@
+// region:    --- Modules
+
+pub mod aip_agent;
+pub mod aip_cmd;
+pub mod aip_code;
+pub mod aip_file;
+pub mod aip_flow;
+pub mod aip_git;
+pub mod aip_hbs;
+pub mod aip_html;
+pub mod aip_json;
+pub mod aip_lua;
+pub mod aip_md;
+pub mod aip_path;
+pub mod aip_rust;
+pub mod aip_semver;
+pub mod aip_text;
+pub mod aip_web;
+
+// endregion: --- Modules
--- src/script/lua_script/mod.rs
+++ src/script/lua_script/mod.rs
@@ -1,25 +1,11 @@
 // region:    --- Modules
 
-mod aip_agent;
-mod aip_cmd;
-mod aip_code;
-mod aip_file;
-mod aip_flow;
-mod aip_git;
-mod aip_hbs;
-mod aip_html;
-mod aip_json;
-mod aip_lua;
-mod aip_md;
-mod aip_path;
-mod aip_rust;
-mod aip_semver;
-mod aip_text;
-mod aip_web;
+mod aip_modules;
 mod helpers;
 mod lua_engine;
 mod lua_value_ext;
 
+pub use aip_modules::*;
 pub use lua_engine::*;
 pub use lua_value_ext::*;
 
