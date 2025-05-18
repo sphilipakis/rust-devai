5ea05d93a01e0c6d3037e26cf3d18d29c0e1de99
Jeremy Chone <jeremy.chone@gmail.com>
2025-05-17 17:10:12 -0700

    . code relayout

--- src/tui/printer/printers/mod.rs
+++ src/tui/printer/printers/mod.rs
@@ -1,18 +1,18 @@
 // region:    --- Modules
 
-mod api_keys;
 mod common;
-mod error_generic;
-mod error_key_env_missing;
-mod pack_list;
+mod print_api_keys;
+mod print_error_generic;
+mod print_error_key_env_missing;
 mod print_info;
+mod print_pack_list;
 
-pub use api_keys::*;
 #[allow(unused)]
 pub use common::*;
-pub use error_generic::*;
-pub use error_key_env_missing::*;
-pub use pack_list::*;
+pub use print_api_keys::*;
+pub use print_error_generic::*;
+pub use print_error_key_env_missing::*;
 pub use print_info::*;
+pub use print_pack_list::*;
 
 // endregion: --- Modules
