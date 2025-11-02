df67917f6a391e08407350f3cdfc23677c5ed758
Jeremy Chone <jeremy.chone@gmail.com>
2025-11-02 10:52:36 -0800

    . typos

--- src/derive_aliases.rs
+++ src/derive_aliases.rs
@@ -1,10 +1,10 @@
 use macro_rules_attribute::derive_alias;
 
 derive_alias! {
-	// When need Ord (if need Hash as well, add it to the derive)
+	// Basic compare (no float)
 	#[derive(Cmp!)] = #[derive(PartialEq, Eq, PartialOrd, Ord)];
 
-	// When just need to have hash only (no ex)
+	// Basic hash (e.g., for hash keys)
 	//#[derive(Hash!)] = #[derive(PartialEq, Eq, Hash)];
 
 	// For enum as str
