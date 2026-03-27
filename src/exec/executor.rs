6f1f2bff3b7c45f95569088115ad97e3cd20e13a
Jeremy Chone <jeremy.chone@gmail.com>
2026-03-26 19:39:07 -0700

    - redo-count - reset manual redo chain to zero and document behavior

--- src/exec/executor.rs
+++ src/exec/executor.rs
@@ -332,11 +332,23 @@ impl Executor {
 			ExecActionEvent::Redo => {
 				if let Some(redo_ctx) = self.take_current_redo_ctx().await {
 					hub.publish(ExecStatusEvent::RunStart).await;
-					let next_flow_redo_count = redo_ctx.flow_redo_count() + 1;
+					let flow_redo_count = if redo_ctx.redo_requested() {
+						redo_ctx.flow_redo_count() + 1
+					} else {
+						0
+					};
+					let redo_ctx = RunRedoCtx::new(
+						redo_ctx.runtime().clone(),
+						redo_ctx.agent().clone(),
+						redo_ctx.run_options().with_flow_redo_count(flow_redo_count),
+						redo_ctx.redo_requested(),
+						flow_redo_count,
+					);
 					// if sucessful, we recapture the redo_ctx to have the latest agent.
 					if let Some(redo_ctx) = exec_run_redo(&redo_ctx).await {
 						let redo_requested = redo_ctx.redo_requested();
 						let next_redo_ctx = if redo_requested {
+							let next_flow_redo_count = redo_ctx.flow_redo_count();
 							let run_options = redo_ctx.run_options().with_flow_redo_count(next_flow_redo_count);
 							RunRedoCtx::new(
 								redo_ctx.runtime().clone(),
